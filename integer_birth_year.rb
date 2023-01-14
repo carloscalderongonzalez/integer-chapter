6cb63212bd44b37d960bd47a1437bab534562c35
ccalderong94 <carloscalderon@chicagobooth.edu>
2023-01-14 23:59:08 +0000

    1.0

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -7,3 +7,10 @@
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+age=gets
+age=age.to_i
+require "date"
+current=Date.today.year
+output=current-age
+output=output.to_s
+p "Wow, you were born in " + output + ". You're old!"
